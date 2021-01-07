e5f4fd3c598a0c330a1b597def2e9d2fdd24ed5e
Guillaume Lazaro <guillaume.lazaro@webinage.fr>
2021-01-07 13:50:58 +0100

    add None + numpy

--- Point.py
+++ Point.py
@@ -1,13 +1,13 @@
 class Point:
 
-    def __init__(self, x, y, z=0):
+    def __init__(self, x, y, z=None):
         self.x = x
         self.y = y
         self.z = z
 
     # Surcharge de l'opérateur
     def __str__(self):
-        if self.z == 0:
+        if self.z == None:
             return "P({} , {})".format(self.x, self.y)
         else:
             return "P({} , {} , {})".format(self.x, self.y, self.z)
